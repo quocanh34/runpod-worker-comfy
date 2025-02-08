325a4658879b8df04712efb9e53c0605fe72a75e
quocanh34 <phamquocanh2002ct@gmail.com>
2025-02-08 21:27:01 +0000

    update path

--- src/restore_snapshot.sh
+++ src/restore_snapshot.sh
@@ -17,8 +17,14 @@ cd custom_nodes/ComfyUI-Manager
 git checkout 22878f4ef848b93f908e2a938e27b09ec0630224
 pip install -r requirements.txt
 
+# Install ComfyUI_Comfyroll_CustomNodes
+cd ..
+git clone https://github.com/ltdrdata/ComfyUI_Comfyroll_CustomNodes.git custom_nodes/ComfyUI_Comfyroll_CustomNodes
+cd ComfyUI_Comfyroll_CustomNodes
+git checkout d78b780ae43fcf8c6b7c6505e6ffb4584281ceca
+
 # Move snapshot file to startup-scripts
-mv /snapshot.json startup-scripts/restore-snapshot.json
+mv /snapshot.json snapshots/restore-snapshot.json
 
 cd ../..
 
